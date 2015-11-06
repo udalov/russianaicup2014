18f980be58688c16c89fbdf2f76d2a63627ea9ae
Alexander Udalov <udalov.alexander@gmail.com>
2015-11-07 01:07:36 +0300

    Update to Kotlin 1.0 Beta

--- local/local.kt
+++ local/local.kt
@@ -1,13 +1,11 @@
 package runner.local
 
-import java.util.ArrayList
 import org.apache.log4j.Logger
-import java.lang.reflect.InvocationTargetException
 import java.io.File
-import java.util.Arrays
-import java.util.Random
-import java.net.URLClassLoader
+import java.lang.reflect.InvocationTargetException
 import java.net.ConnectException
+import java.net.URLClassLoader
+import java.util.*
 
 val LOG_FILE = "out/log.txt"
 
@@ -15,8 +13,8 @@ open class Player(val name: String, val classFile: String)
 object MyStrategy : Player("MyStrategy", "#LocalTestPlayer")
 object BootstrapStrategy : Player("Bootstrap", "#LocalTestPlayer")
 object KeyboardPlayer : Player("KeyboardPlayer", "#KeyboardPlayer")
-object EmptyPlayer : Player("EmptyPlayer", javaClass<com.a.b.a.a.e.a>().getSimpleName() + ".class")
-object QuickStartGuy : Player("QuickStartGuy", javaClass<com.a.b.a.a.e.b>().getSimpleName() + ".class")
+object EmptyPlayer : Player("EmptyPlayer", com.a.b.a.a.e.a::class.java.simpleName + ".class")
+object QuickStartGuy : Player("QuickStartGuy", com.a.b.a.a.e.b::class.java.simpleName + ".class")
 
 fun localRunner(vis: Boolean, ticks: Int, seed: Long, teamSize: Int, players: List<Player>): Runnable {
     Logger.getRootLogger()?.removeAllAppenders()
@@ -34,57 +32,57 @@ fun localRunner(vis: Boolean, ticks: Int, seed: Long, teamSize: Int, players: Li
             "-plugins-directory=out/production/plugins"
     )
 
-    for ((index, player) in players.withIndices()) {
+    for ((index, player) in players.withIndex()) {
         val i = index + 1
         args.add("-p$i-name=${player.name}")
         args.add("-p$i-team-size=$teamSize")
         args.add(player.classFile)
     }
 
-    return com.a.b.c(args.copyToArray())
+    return com.a.b.c(args.toTypedArray())
 }
 
 fun runGame(vis: Boolean, ticks: Int, seed: Long, teamSize: Int, players: List<Player>) {
     val threads = ArrayList<Thread>(2)
-    threads add Thread(localRunner(vis, ticks, seed, teamSize, players))
+    threads.add(Thread(localRunner(vis, ticks, seed, teamSize, players)))
 
     var nextPort = 31001L
     for (player in players) {
         if (player == MyStrategy) {
             val port = nextPort++
-            threads add Thread {
-                Thread.currentThread().setName(if (vis) "local-vis" else "local")
+            threads.add(Thread {
+                Thread.currentThread().name = if (vis) "local-vis" else "local"
                 val runnerClass = Class.forName("Runner")
                 runMyStrategy(runnerClass, port)
                 val score = runnerClass.getDeclaredMethod("getScore").invoke(null) as IntArray
                 val outcome = if (score[0] > score[1]) "WIN" else if (score[0] < score[1]) "LOSE" else "DRAW"
                 println("$outcome ${score[0]}:${score[1]}")
-            }
+            })
         }
         else if (player == BootstrapStrategy) {
             val port = nextPort++
-            val classLoader = URLClassLoader(array(File("out/bootstrap").toURI().toURL()), null)
+            val classLoader = URLClassLoader(arrayOf(File("out/bootstrap").toURI().toURL()), null)
             val runnerClass = classLoader.loadClass("Runner")!!
-            threads add Thread {
+            threads.add(Thread {
                 runMyStrategy(runnerClass, port)
-            }
+            })
         }
     }
 
-    threads forEach { it.start() }
-    threads forEach { it.join() }
+    threads.forEach { it.start() }
+    threads.forEach { it.join() }
 }
 
 fun runMyStrategy(runnerClass: Class<*>, port: Long) {
-    val main = runnerClass.getDeclaredMethod("main", javaClass<Array<String>>())
+    val main = runnerClass.getDeclaredMethod("main", Array<String>::class.java)
     while (true) {
         try {
-            main(null, array("127.0.0.1", "$port", "0000000000000000"))
+            main(null, arrayOf("127.0.0.1", "$port", "0000000000000000"))
         } catch (e: InvocationTargetException) {
-            if (e.getTargetException() is ConnectException) {
+            if (e.targetException is ConnectException) {
                 Thread.sleep(40)
                 continue
-            } else throw e.getTargetException() ?: e
+            } else throw e.targetException ?: e
         }
         break
     }
